1b2d8444620ed2a3556413f6f58f3ee9c4933f38
Sumit Bhardwaj <40798467+nukeworm@users.noreply.github.com>
2019-08-03 15:56:25 +0530

    Update timeConversion.c

--- timeConversion.c
+++ timeConversion.c
@@ -7,11 +7,15 @@ int main(){
 	int n=10,hr;
 	char time[n],mer[2];
 	scanf("%s",&time);
+	
 	//get the meridian in 'mer'
 	strcpy(mer,&time[8]);
 	mer[2]='\0';
+	
+	//remove meridian from 'time'
 	strncpy(time,time,8);
 	time[8]='\0';
+	
 	//AM
 	if(mer[0]=='A'){
 		if(((time[0]-'0')*10)+(time[1]-'0')==12){
@@ -23,8 +27,8 @@ int main(){
 	//PM
 	else{
 		if(((time[0]-'0')*10)+(time[1]-'0')==12){
-            time[0]=1+'0';
-            time[1]=2+'0';
+            time[0]='1';
+            time[1]='2';
             printf("%s",time);
         }
         else{
